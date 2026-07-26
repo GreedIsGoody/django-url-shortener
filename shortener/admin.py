aec204bbbe070beadafe3ac3aa4113b395ec70c2
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-07-26 13:27:57 +0300

    url shortener logic completed and admin superuser creation

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ShortenerURL
 
-# Register your models here.
+
+@admin.register(ShortenerURL)
+class ShortenerURLAdmin(admin.ModelAdmin):
+    
+    #Fields what will display in table
+    list_display = ("short_code", "original_url", "clicks_count", "created_at")
+    
+    
+    #Search fields
+    search_fields = ("short_code", "original_url")
+    
+    #Filters to the fields
+    list_filter = ("created_at",)
+    
+    #Only for reading fields
+    readonly_fields = ("clicks_count", "created_at")
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import ShortenerURL
 
-# Create your views here.
+
+@csrf_exempt
+def create_short_url(request):
+    
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            original_url = data.get("url")
+            
+            if not original_url:
+                return JsonResponse({"error": "URL is required"}, status=400)
+            
+            link = ShortenerURL.objects.create(original_url=original_url)
+            
+            short_url = request.build_absolute_uri(f"/r/{link.short_code}")
+            
+            return JsonResponse({
+                "short_code": link.short_code,
+                "short_url" : short_url,
+                "original_url": link.original_url
+            }, status=201)
+            
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+        
+    return HttpResponseBadRequest("Only POST request are allowed")
+
+def redirect_to_original(request, short_code):
+    link = get_object_or_404(ShortenerURL, short_code=short_code)
+    
+    link.clicks_count += 1
+    link.save(update_fields=["clicks_count"])
+    
+    return redirect(link.original_url)
